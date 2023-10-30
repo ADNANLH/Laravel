0f38f4e7c11051b3c37108647c0489ee18704ec6
ADNANLH <adnanharrak5@gmail.com>
2023-10-30 16:17:32 +0100

    routing 30-10

--- Laravel10/routes/web.php
+++ Laravel10/routes/web.php
@@ -15,12 +15,21 @@ use Illuminate\Http\Request;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-// lancer server 
+
+//  chapitre L'ORM  
+
+    //  communiquer avec une base  de donnee avec laravel  
+    // l'orem c'st le demunitif de Object Relationnel Mapping  
+    // ce  sont des classes qui vont  permettre d'interagir avec les donnee en BD qui vont permettre  de  les representent  sous forme d'objet 
+// a la place de creer  la table et creer les infos d'interieur  dans la ravel on a : 
+// - on va utiliser le systeme  de migration 
+// creer une migration qui va permettre de rajouter les infos  dans notre BD (nomme notre migration)
+// php artisan make:migration create_admins_table 
 
 
 
 
-   // ROUTING
+
 
 Route::get('/', function () {
     return view('welcome');
