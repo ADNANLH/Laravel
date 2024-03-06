91e73f69df978e6ba565f028a06d019f0b339b60
ADNANLH <adnanharrak5@gmail.com>
2024-03-06 16:51:17 +0100

    authentification

--- 8-formulaires/routes/web.php
+++ 8-formulaires/routes/web.php
@@ -2,6 +2,7 @@
 
 // request  
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use App\Models\posts;
 use Illuminate\Http\Request; 
@@ -12,20 +13,21 @@ use Illuminate\Support\Facades\Route;
  *  Par exemple, supposons que nous avons une route qui prend un ID et un slug comme paramètres
 */
 
-Route::get('/', function () {
-     return view('welcome');
-});
 
 // Le modèle binding nous permet de lier un paramètre de route à un 
 // modèle de base de données. Par exemple, supposons que nous avons une route qui
 //  prend un ID et un slug comme paramètres.
+Route::get('/', function () {
+     return view('welcome');
+});
+
 
 Route::prefix('/blog')->name('blog.')->controller(BlogController::class)->group(function (){
     Route::get('/', 'index')->name('index');
-    Route::get('/new', 'create')->name('create');
-    Route::post('/new', 'store');
-    Route::get('/{post}/edit', 'edit')->name('edit');
-    Route::patch('/{post}/edit', 'update');
+    Route::get('/new', 'create')->name('create')->middleware('auth');
+    Route::post('/new', 'store')->middleware('auth');
+    Route::get('/{post}/edit', 'edit')->name('edit')->middleware('auth');
+    Route::patch('/{post}/edit', 'update')->middleware('auth');
     Route::get('/{slug}-{post}', 'show')->where([
         'post'=> '[0-9]+',
         'slug'=> '[a-z0-9\-]+'
